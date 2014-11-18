de6ada88d46efb006b796651a41bc7dc8b693f05
KCioffiJr1014@gmail.com <KCioffiJr1014@gmail.com>
2014-11-18 10:38:32 -0500

    Stuff for Butterfly

--- Butterfly.py
+++ Butterfly.py
@@ -2,7 +2,22 @@ import pygame, math
 
 class Butterfly():
 	def __init__(self, image, speed = [0,0], pos = [0,0]):
-	
+		self.surface = pygame.image.load("rsc/Butterfly/Butterfly.png")
+        self.surface = pygame.transform.scale(self.surface,(15,25))
+        self.decras = 100
+        self.rect = self.surface.get_rect()
+        self.maxSpeed = 2
+        xDir = random.randint(-1,1)
+        yDir = random.randint(-1,1)
+        self.speed = [self.maxSpeed*xDir,self.maxSpeed*yDir]    
+        self.place(pos)
+        self.damage = 1
+        self.life = 20
+        self.maxLife = 20
+        self.radius = 10
+        self.detectionRadius = 80
+        self.didHit = False
+        self.living = True
 		
 	def place(self, pos):
 		self.rect.center = pos
