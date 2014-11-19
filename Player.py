c67c1f1331ec8fa68d3c3e567af11ca7c7f5ce6b
Jacob <jacob.lipscomb@kearsarge.org>
2014-11-19 08:51:36 -0500

    Added Player code

--- Player.py
+++ Player.py
@@ -1,29 +1,94 @@
 import pygame, math
 
 class Player():
-	def __init__(self, image, speed = [0,0], pos = [0,0]):
-	
-		
-	def place(self, pos):
-		self.rect.center = pos
-		
-	def update(self, width, height):
-		
-		
-	def move(self):
-		self.rect = self.rect.move(self.speed)
-
-	def collideWall(self, width, height):
-		
-		
-	def collideBall(self, other):
-		
-	def distance(self, pt):
-		x1 = self.rect.center[0]
-		y1 = self.rect.center[1]
-		x2 = pt[0]
-		y2 = pt[1]
-		return math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
+    def __init__(self, pos):
+        Ball.__init__(self, "images/Player/Back.png", [0,0], pos)
+        self.upImages = [pygame.image.load("images/Player/MU.png"),
+						 pygame.image.load("images/Player/MU2.png")]
+        self.downImages = [pygame.image.load("images/Player/MD.png"),
+                           pygame.image.load("images/Player/MD2.png")]
+        self.leftImages = [pygame.image.load("images/Player/ML.png"),
+                           pygame.image.load("images/Player/ML2.png")]
+        self.rightImages = [pygame.image.load("images/Player/MR.png"),
+                            pygame.image.load("images/Player/MR2.png")]
+        self.facing = "up"
+        self.changed = False
+        self.images = self.upImages
+        self.frame = 0
+        self.maxFrame = len(self.images) - 1
+        self.waitCount = 0
+        self.maxWait = 60*.9
+        self.image = self.images[self.frame]
+        self.rect = self.image.get_rect(center = self.rect.center)
+        self.maxSpeed = 10
+            
+    def update(self, width, height):
+        Ball.update(self, width, height)
+        self.animate()
+        self.facingChanged = False
+        
+    def collideWall(self, width, height):
+        if not self.didBounceX:
+            #print "trying to hit Wall"
+            if self.rect.left < 0 or self.rect.right > width:
+                self.speedx = 0
+                self.didBounceX = True
+                #print "hit xWall"
+        if not self.didBounceY:
+            if self.rect.top < 0 or self.rect.bottom > height:
+                self.speedy = 0
+                self.didBounceY = True
+                #print "hit xWall"
+    
+    def animate(self):
+        if self.waitCount < self.maxWait:
+            self.waitCount += 1
+        else:
+            self.waitCount = 0
+            self.facingChanged = True
+            if self.frame < self.maxFrame:
+                self.frame += 1
+            else:
+                self.frame = 0
+        
+        if self.changed:    
+            if self.facing == "up":
+                self.images = self.upImages
+            elif self.facing == "down":
+                self.images = self.downImages
+            elif self.facing == "right":
+                self.images = self.rightImages
+            elif self.facing == "left":
+                self.images = self.leftImages
+            
+            self.image = self.images[self.frame]
+    
+    def go(self, direction):
+        if direction == "up":
+            self.facing = "up"
+            self.changed = True
+            self.speedy = -self.maxSpeed
+        elif direction == "stop up":
+            self.speedy = 0
+        elif direction == "down":
+            self.facing = "down"
+            self.changed = True
+            self.speedy = self.maxSpeed
+        elif direction == "stop down":
+            self.speedy = 0
+            
+        if direction == "right":
+            self.facing = "right"
+            self.changed = True
+            self.speedx = self.maxSpeed
+        elif direction == "stop right":
+            self.speedx = 0
+        elif direction == "left":
+            self.facing = "left"
+            self.changed = True
+            self.speedx = -self.maxSpeed
+        elif direction == "stop left":
+            self.speedx = 0
 		
 		
 		
