61ee0b5412bb9b43816081e699121a31c1ca59bb
noxiss <noxiss2015@gmail.com>
2015-01-26 09:15:50 -0500

    start menu work

--- Menu.py
+++ Menu.py
@@ -1,27 +1,42 @@
-import pygame, sys, math
+import pygame
 
-class Button:
-    
-    def __init__(self, text, location, color, highlighted = False):
-        self.surfaces = pygame.image.load ("rsc/Startscreen/startscreen")
-        self.surface = self.font.render(str(self.text), 1, color)
-        self.frame = 0
-        self.rect = self.surface.get_rect()
-        self.clicked = False
-        self.highlighted = highlighted
-        self.place(location)
+class Button():
+        def __init__(self, pos, image, clickedImage = "" ):
+                if clickedImage != "":
+                        self.baseImage =  pygame.image.load(image)
+                        self.clickedImage =  pygame.image.load(clickedImage)
+                else:
+                        self.baseImage =  pygame.image.load(image)
+                        self.clickedImage =  pygame.image.load(image)
+                self.image = self.baseImage
+                self.rect = self.image.get_rect()
+                self.place(pos)
+                self.clicked = False
+                
+        def place(self, pos):
+                self.rect.center = pos
+                
+        def collidePoint(self, pt):
+                if self.rect.right > pt[0] and self.rect.left < pt[0]:
+                        if self.rect.bottom > pt[1] and self.rect.top < pt[1]:          
+                                return True
+                return False
         
-    def place(self, pt):
-        self.rect = self.rect.move(pt)    
+        def click(self, pt):
+                if self.collidePoint(pt):
+                        self.clicked = True
+                        self.image = self.clickedImage
+                else:
+                        self.clicked = False
+                        self.image = self.baseImage
+                        
+        def release(self, pt):
+                if self.clicked and self.collidePoint(pt):
+                        return True
+                else:
+                        self.clicked = False
+                        self.image = self.baseImage
+                        return False
         
-    def update(self, color):
-        if self.highlighted:
-            self.frame = 1
-        else:
-            self.frame = 0
-            self.surface = self.font.render(str(self.text), 1, (color))
-        
-    def collidePt(self, pt):
-        if (pt[0] > self.rect.left and pt[0] < self.rect.right):
-            if (pt[1] > self.rect.top and pt[1] < self.rect.bottom):
-                return True
+        def update(self, width, height):
+                pass
