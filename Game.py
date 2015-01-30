ed4eb143864207600579a5defaa05e68bea2110f
noxiss <noxiss2015@gmail.com>
2015-01-30 09:27:34 -0500

    start menu

--- Game.py
+++ Game.py
@@ -17,6 +17,8 @@ height = 600
 fullscreen = 0
 altFlag = False
 size = width, height
+screen = pygame.display.set_mode(size)
+
 bgColor = r, g, b = 0, 0, 0
 bg = pygame.image.load("rsc/Background/BG2.png")
 bgRect = bg.get_rect()
@@ -24,219 +26,183 @@ bgImage = pygame.image.load("rsc/Startscreen/startscreen.png")
 bgRect = bgImage.get_rect()
 
 startButton = Button([width/4, height-300], 
-					"rsc/Startscreen/start.png",
-					"rsc/Startscreen/starthighlighted.png")
+                    "rsc/Startscreen/start.png",
+                    "rsc/Startscreen/starthighlighted.png")
                                      
 startButton2 = Button([width/2, height-300],
-					"rsc/Startscreen/options.png",
-					"rsc/Startscreen/optionshighlighted.png")
-									
-StartButton3 = Button([{width/4}*3, height-300],
-					"rsc/Startscreen/quit.png",
-					"rsc/Startscreen/quit.png")
+                    "rsc/Startscreen/options.png",
+                    "rsc/Startscreen/optionshighlighted.png")
+                                    
+startButton3 = Button([width/4.0*3, height-300],
+                    "rsc/Startscreen/quit.png",
+                    "rsc/Startscreen/quit.png")
                                      
 startCharacter = pygame.image.load("rsc/Startscreen/startscreen.png",
                                 "rsc/Startscreen/startscreen.png")
 
-while True:
-	while not running:
-		for event in pygame.event.get():
-			if event.type == pygame.quit: sys.exit()
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RETURN:
-					running = True
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				startButton.click(event.pos)
-			if event.type == pygame.MOUSEBUTTONUP:
-				if startButton.release(event.pos):
-					running = True
+running = False
 
-'''
-background = Screen(["rsc/Player/MD.png"], [0,0], size, 10)
-singleplayer = Button("Play", [350, 175], (0, 100, 100))
-endscreen = Screen(["rsc/Player/deadscreen.png"], [0,0], size, 10)
-#MENU----------------
 while True:
-    while not run and player.living:
-        #print run, player.living
+    while not running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if (event.key == pygame.K_UP or event.key == pygame.K_w) and not singleplayer.highlighted:
-                    singleplayer.highlighted = True
-                    exit.highlighted = False
-                elif event.key == pygame.K_DOWN or event.key == pygame.K_s and not exit.highlighted:
-                    exit.highlighted = True
-                    singleplayer.highlighted = False
-                elif event.key == pygame.K_SPACE and exit.highlighted == True:
-                    sys.exit()
-                elif event.key == pygame.K_SPACE and singleplayer.highlighted == True:
-                    run = True
-            elif event.type == pygame.MOUSEMOTION:
-                if singleplayer.collidePt(event.pos):
-                    singleplayer.highlighted = True
-                    exit.highlighted = False
-                elif exit.collidePt(event.pos):
-                    singleplayer.highlighted = False
-                    exit.highlighted = True
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    if singleplayer.collidePt(event.pos):
-                        singleplayer.clicked = True
-                        run = True
-                    elif exit.collidePt(event.pos):
-                        exit.clicked = True
-                        sys.exit()
-                        
-#----------------------------------------------------
-        singleplayer.update((200, 10, 10))
-        exit.update((200, 10, 10))
+            if event.type == pygame.quit: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    running = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                startButton.click(event.pos)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if startButton.release(event.pos):
+                    running = True
         
         screen.fill(bgColor)
-        screen.blit(singleplayer.surface, singleplayer.rect)
-        screen.blit(exit.surface, exit.rect)
-        pygame.display.flip()'''
+        screen.blit(bgImage, bgRect)
+        screen.blit(startButton.image, startButton.rect)
+        screen.blit(startButton2.image, startButton2.rect)
+        screen.blit(startButton3.image, startButton3.rect)
+        #screen.blit(startCharacter.image, startCharacter.rect)
+        pygame.display.flip()
+        
+    
+
+
+    #timer = Score([80, height - 25], "Time: ", 36)
+    #timerWait = 0
+    #timerWaitMax = 6
+
+    #Score = Score([80, height - 50], "Score: ", 36)
+    #if Butterfly = self.living = True:
+    #Score = Score + 100
 
 
 
-#timer = Score([80, height - 25], "Time: ", 36)
-#timerWait = 0
-#timerWaitMax = 6
 
-#Score = Score([80, height - 50], "Score: ", 36)
-#if Butterfly = self.living = True:
-#Score = Score + 100
+    player = Player([width / 2, height / 2])
+    #75 for both
+    healthbar = HealthBar([width - 75, 125])  #DEFAULT: 100 MODED: 200
+    #600
 
+    butterflys = []
+    butterflys += [Butterfly("rsc/Butterfly/Butterfly.png", [1, 2], [100, 125])]
 
+    wasps = []
+    wasps += [Wasp("rsc/Wasp/Wasp.png", [1, 2], [100, 125])]
 
-screen = pygame.display.set_mode(size)
+    projectiles = []
 
-player = Player([width / 2, height / 2])
-#75 for both
-healthbar = HealthBar([width - 75, 125])  #DEFAULT: 100 MODED: 200
-#600
+    while running:
+        st = time.time()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    player.go("up")
+                elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    player.go("right")
+                elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    player.go("down")
+                elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    player.go("left")
+                elif (event.key == pygame.K_e or event.key == pygame.K_j):
+                    projectiles += [player.attack("gust")]
+                elif (event.key == pygame.K_RALT or event.key == pygame.K_LALT):
+                    altFlag = True
+                elif (event.key == pygame.K_RETURN) and altFlag:
+                    if fullscreen == 0:
+                        fullscreen = pygame.FULLSCREEN
+                    else:
+                        fullscreen = 0
+                    screen = pygame.display.set_mode((width, height), fullscreen)
+                    pygame.display.flip()
 
-butterflys = []
-butterflys += [Butterfly("rsc/Butterfly/Butterfly.png", [1, 2], [100, 125])]
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    player.go("stop up")
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    player.go("stop right")
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    player.go("stop down")
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    player.go("stop left")
+                elif (event.key == pygame.K_j ):
+                    player.attack("stop gust")
+                elif (event.key == pygame.K_RALT or event.key == pygame.K_LALT):
+                    altFlag = False
 
-wasps = []
-wasps += [Wasp("rsc/Wasp/Wasp.png", [1, 2], [100, 125])]
+        #print "controls:", time.time() - st
+        if len(butterflys) < 10:
+            if random.randint(0, 1 * 60) == 0:
+                butterflys += [Butterfly("rsc/Butterfly/Butterfly2.png",
+                                         [random.randint(0, 10), random.randint(0, 10)],
+                                         [random.randint(100, width - 100), random.randint(100, height - 100)]
+                )]
 
-projectiles = []
-
-while True:
-    st = time.time()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w or event.key == pygame.K_UP:
-                player.go("up")
-            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                player.go("right")
-            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                player.go("down")
-            elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                player.go("left")
-            elif (event.key == pygame.K_e or event.key == pygame.K_j):
-                projectiles += [player.attack("gust")]
-            elif (event.key == pygame.K_RALT or event.key == pygame.K_LALT):
-                altFlag = True
-            elif (event.key == pygame.K_RETURN) and altFlag:
-                if fullscreen == 0:
-                    fullscreen = pygame.FULLSCREEN
-                else:
-                    fullscreen = 0
-                screen = pygame.display.set_mode((width, height), fullscreen)
-                pygame.display.flip()
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w or event.key == pygame.K_UP:
-                player.go("stop up")
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                player.go("stop right")
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                player.go("stop down")
-            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                player.go("stop left")
-            elif (event.key == pygame.K_j ):
-                player.attack("stop gust")
-            elif (event.key == pygame.K_RALT or event.key == pygame.K_LALT):
-                altFlag = False
-
-    #print "controls:", time.time() - st
-    if len(butterflys) < 10:
-        if random.randint(0, 1 * 60) == 0:
-            butterflys += [Butterfly("rsc/Butterfly/Butterfly2.png",
-                                     [random.randint(0, 10), random.randint(0, 10)],
-                                     [random.randint(100, width - 100), random.randint(100, height - 100)]
-            )]
-
-    if len(wasps) < 10:
-        if random.randint(0, 5 * 60) == 0:
-            wasps += [Wasp("rsc/Wasp/Wasp.png",
-                           [random.randint(0, 10), random.randint(0, 10)],
-                           [random.randint(100, width - 100), random.randint(100, height - 100)]
-            )]
-            #if timerWait < timerWaitMax:
-            #   timerWait += 1
-            #else:
-            #   timerWait = o
-            #  timer.increaseScore(.1)
-    player.update(width, height)
-    #timer.update()
-    #print "spawn:", time.time() - st
-    player.update(width, height)
-    for butterfly in butterflys:
-        butterfly.update(width, height)
-    for wasp in wasps:
-        wasp.update(width, height, player)
-    for projectile in projectiles:
-        projectile.update(width, height)
-
-    #print "update:", time.time() - st
-    for bully in butterflys:
-        for victem in butterflys:
-            bully.collideButterfly(victem)
-            bully.collidePlayer(player)
-    for bully in wasps:
-        for victem in wasps:
-            bully.collideWasp(victem)
-        bully.collidePlayer(player)
+        if len(wasps) < 10:
+            if random.randint(0, 5 * 60) == 0:
+                wasps += [Wasp("rsc/Wasp/Wasp.png",
+                               [random.randint(0, 10), random.randint(0, 10)],
+                               [random.randint(100, width - 100), random.randint(100, height - 100)]
+                )]
+                #if timerWait < timerWaitMax:
+                #   timerWait += 1
+                #else:
+                #   timerWait = o
+                #  timer.increaseScore(.1)
+        player.update(width, height)
+        #timer.update()
+        #print "spawn:", time.time() - st
+        player.update(width, height)
+        for butterfly in butterflys:
+            butterfly.update(width, height)
+        for wasp in wasps:
+            wasp.update(width, height, player)
         for projectile in projectiles:
-            bully.collide_attack(projectile)
-            #projectile.collide_wasp(bully)
+            projectile.update(width, height)
 
-    #print "collide:", time.time() - st
-    for butterfly in butterflys:
-        if not butterfly.living:
-            butterflys.remove(butterfly)
-    for wasp in wasps:
-        if not wasp.living:
-            wasps.remove(wasp)
+        #print "update:", time.time() - st
+        for bully in butterflys:
+            for victem in butterflys:
+                bully.collideButterfly(victem)
+                bully.collidePlayer(player)
+        for bully in wasps:
+            for victem in wasps:
+                bully.collideWasp(victem)
+            bully.collidePlayer(player)
+            for projectile in projectiles:
+                bully.collide_attack(projectile)
+                #projectile.collide_wasp(bully)
 
-    if player.health <= 0:
-        player.living = False
+        #print "collide:", time.time() - st
+        for butterfly in butterflys:
+            if not butterfly.living:
+                butterflys.remove(butterfly)
+        for wasp in wasps:
+            if not wasp.living:
+                wasps.remove(wasp)
 
-    #print "die:", time.time() - st
-    bgColor = r, g, b
-    screen.fill(bgColor)
-    screen.blit(bg, bgRect)
-    screen.blit(healthbar.surface, healthbar.rect)
-    for butterfly in butterflys:
-        screen.blit(butterfly.image, butterfly.rect)
-        player.collideWasp(healthbar)
-    for wasp in wasps:
-        screen.blit(wasp.image, wasp.rect)
-    #for wasphealthbar in wasphealthbar:
-     #   screen.blit(wasphealthbar.image, wasphealthbar.rect)
-    screen.blit(player.image, player.rect)
-    #screen.blit(timer.image, timer.rect)
-    #screen.blit(Score.image, Score.rect)
-    for projectile in projectiles:
-        screen.blit(projectile.image, projectile.rect)
-    pygame.display.flip()
-    #print "draw:", time.time() - st
-    clock.tick(60)
+        if player.health <= 0:
+            player.living = False
+
+        #print "die:", time.time() - st
+        bgColor = r, g, b
+        screen.fill(bgColor)
+        screen.blit(bg, bgRect)
+        screen.blit(healthbar.surface, healthbar.rect)
+        for butterfly in butterflys:
+            screen.blit(butterfly.image, butterfly.rect)
+            player.collideWasp(healthbar)
+        for wasp in wasps:
+            screen.blit(wasp.image, wasp.rect)
+        #for wasphealthbar in wasphealthbar:
+         #   screen.blit(wasphealthbar.image, wasphealthbar.rect)
+        screen.blit(player.image, player.rect)
+        #screen.blit(timer.image, timer.rect)
+        #screen.blit(Score.image, Score.rect)
+        for projectile in projectiles:
+            screen.blit(projectile.image, projectile.rect)
+        pygame.display.flip()
+        #print "draw:", time.time() - st
+        clock.tick(60)
 
 
 
--- Menu.py
+++ Menu.py
@@ -1,57 +1,38 @@
 import pygame
 
-
-class Button():
-        def __init__(self, pos, image, clickedImage = "" ):
-                if clickedImage != "":
-                        self.baseImage =  pygame.image.load(image)
-                        self.clickedImage =  pygame.image.load(clickedImage)
-                else:
-                        self.baseImage =  pygame.image.load(image)
-                        self.clickedImage =  pygame.image.load(image)
-                self.image = self.baseImage
-                self.rect = self.image.get_rect()
-                self.place(pos)
-                self.clicked = False
-                
-        def place(self, pos):
-                self.rect.center = pos
-                
-        def collidePoint(self, pt):
-                if self.rect.right > pt[0] and self.rect.left < pt[0]:
-                        if self.rect.bottom > pt[1] and self.rect.top < pt[1]:          
-                                return True
-                return False
-
 class Button:
-    
-    def __init__(self, text, location, color, highlighted = False):
-        self.surfaces = []
-        self.font = pygame.font.Font(None, 60)
-        self.text = text
-        self.surface = self.font.render(str(self.text), 1, color)
-        self.frame = 0
-        self.rect = self.surface.get_rect()
+    def __init__(self, location, unHighlighted,  highlighted):
+        self.unHighlightedImage = pygame.image.load(unHighlighted)
+        self.highlightedImage = pygame.image.load(highlighted)
+        self.image = self.unHighlightedImage
+        self.rect = self.image.get_rect()
         self.clicked = False
-        self.highlighted = highlighted
         self.place(location)
 
+    def place(self, pos):
+                self.rect.center = pos
+    
+    def click(self, pt):
+        if self.collidePoint(pt):
+            self.clicked = True
+            self.image = self.highlightedImage
+        else:
+            self.clicked = False
+            self.image = self.unHighlightedImage
+           
+    def collidePoint(self, pt):
+        if self.rect.right > pt[0] and self.rect.left < pt[0]:
+            if self.rect.bottom > pt[1] and self.rect.top < pt[1]:          
+                return True
+        return False
+                
+    def release(self, pt):
+        if self.clicked and self.collidePoint(pt):
+                return True
+        else:
+                self.clicked = False
+                self.image = self.unHighlightedImage
+                return False
         
-        def click(self, pt):
-                if self.collidePoint(pt):
-                        self.clicked = True
-                        self.image = self.clickedImage
-                else:
-                        self.clicked = False
-                        self.image = self.baseImage
-                        
-        def release(self, pt):
-                if self.clicked and self.collidePoint(pt):
-                        return True
-                else:
-                        self.clicked = False
-                        self.image = self.baseImage
-                        return False
-        
-        def update(self, width, height):
-                pass
+    def update(self, width, height):
+        pass
