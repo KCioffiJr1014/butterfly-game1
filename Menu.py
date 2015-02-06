9c171505c250bef756e2a99393f1f9e1ce6375a3
noxiss <noxiss2015@gmail.com>
2015-02-06 09:01:39 -0500

    .

--- Menu.py
+++ Menu.py
@@ -1,28 +1,14 @@
 import pygame
 
 
-class Button():
-        def __init__(self, pos, image, clickedImage = "" ):
-                if clickedImage != "":
-                        self.baseImage =  pygame.image.load(image)
-                        self.clickedImage =  pygame.image.load(clickedImage)
-                else:
-                        self.baseImage =  pygame.image.load(image)
-                        self.clickedImage =  pygame.image.load(image)
-                self.image = self.baseImage
-                self.rect = self.image.get_rect()
-                self.place(pos)
-                self.clicked = False
-                
-        def place(self, pos):
-                self.rect.center = pos
-                
-        def collidePoint(self, pt):
-                if self.rect.right > pt[0] and self.rect.left < pt[0]:
-                        if self.rect.bottom > pt[1] and self.rect.top < pt[1]:          
-                                return True
-                return False
-
+class Button:
+    def __init__(self, location, unHighlighted,  highlighted):
+        self.unHighlightedImage = pygame.image.load(unHighlighted)
+        self.highlightedImage = pygame.image.load(highlighted)
+        self.image = self.unHighlightedImage
+        self.rect = self.image.get_rect()
+        self.clicked = False
+        self.place(location)
 
         
         def click(self, pt):
@@ -40,21 +26,30 @@ class Button():
                         self.clicked = False
                         self.image = self.baseImage
                         return False
+    def place(self, pos):
+                self.rect.center = pos
     
+    def click(self, pt):
+        if self.collidePoint(pt):
+            self.clicked = True
+            self.image = self.highlightedImage
+        else:
+            self.clicked = False
+            self.image = self.unHighlightedImage
            
-        def collidePoint(self, pt):
-            if self.rect.right > pt[0] and self.rect.left < pt[0]:
-                if self.rect.bottom > pt[1] and self.rect.top < pt[1]:          
-                    return True
-            return False
+    def collidePoint(self, pt):
+        if self.rect.right > pt[0] and self.rect.left < pt[0]:
+            if self.rect.bottom > pt[1] and self.rect.top < pt[1]:          
+                return True
+        return False
                 
-        def release(self, pt):
-            if self.clicked and self.collidePoint(pt):
-                    return True
-            else:
-                    self.clicked = False
-                    self.image = self.unHighlightedImage
-                    return False
+    def release(self, pt):
+        if self.clicked and self.collidePoint(pt):
+                return True
+        else:
+                self.clicked = False
+                self.image = self.unHighlightedImage
+                return False
         
-        def update(self, width, height):
-            pass
+    def update(self, width, height):
+        pass
